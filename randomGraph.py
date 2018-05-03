01584c4c2171926202b0510b78ba32ca925a9c26
sterritm <sterritm@users.noreply.github.com>
2018-05-03 16:30:10 -0700

    added manual graph creation

--- randomGraph.py
+++ randomGraph.py
@@ -3,23 +3,23 @@ import json
 
 
 
-def addNode(start, size, edges):
+def addRandomNode(start, size, edges):
     node = {}
     node["title"] = "title" + str(start)
-    node["found"] = 0
+    node["found"] = False
     node["edges"] = []
 
     ##select random number of edged
     numEdges = random.randint(0, edges)
     for i in range(numEdges):
-        edge = random.randint(0, size)
+        edge = str(random.randint(0, size))
         while edge in node["edges"]:
-            edge = random.randint(0, size)
+            edge = str(random.randint(0, size))
         node["edges"].append(edge)
     return node
 
 def randomGraph():
-    random.seed(0)
+    random.seed()
     testNum = random.randint(0, 10000)
 
     ##initiate graph
@@ -32,7 +32,7 @@ def randomGraph():
     if size == 0:
         return json.dumps(graph, indent=4, sort_keys=True)
     else:
-        graph["start"] = random.randint(0, size - 1)
+        graph["start"] = str(random.randint(0, size - 1))
 
     edges = int(raw_input("Enter maximum number of edges per node: "))
     if edges > size:
@@ -41,9 +41,60 @@ def randomGraph():
 
     ## add nodes to json
     for i in range(size):
-        graph["URLs"][i] = addNode(i, size, edges)
+        graph["URLs"][i] = addRandomNode(i, size, edges)
 
     return json.dumps(graph, indent=4, sort_keys=True)
-        
 
-print randomGraph()
+def addNode(start, size):
+    node = {}
+    node["title"] = "title" + str(start)
+    node["edges"] = []
+
+    numEdges = int(raw_input("Enter number of edges for node " + str(start) + ": "))
+    for i in range(numEdges):
+        edge = int(raw_input("Enter edge number " + str(i) + " (range 0-" + str(size-1) + "): "))
+        node["edges"].append(edge)
+
+    resp = raw_input("Was the keyword found on this node (y/n): ")
+    if resp == 'y' or resp == 'Y':
+        node['found'] = True
+    else:
+        node['found'] = False
+    return node
+    
+def createGraph():
+    ##initiate graph
+    unit = raw_input("Enter name of test for cookie: ")
+    graph = {}
+    graph["start"] = ""
+    graph["URLs"] = {}
+    graph["cookie"] = unit
+
+    size = int(raw_input("Enter number of nodes in graph: "))
+    if size == 0:
+        return json.dumps(graph, indent=4, sort_keys=True)
+    else:
+        graph["start"] = "0"
+
+    for i in range(size):
+        graph["URLs"][int(i)] = addNode(i, size)
+
+    return json.dumps(graph, indent=4, sort_keys=True)
+    
+def main():
+    random.seed()
+    again = "y"
+    while again == "y" or again == "Y":
+        print "Select the type of graph you want to create:"
+        print "1) Manually create graph"
+        print "2) Create a random graph"
+        choice = raw_input("> ")
+        print ""
+        if choice == "1":
+            print createGraph()
+        elif choice == "2":
+            print randomGraph()
+        print "Continue? (Y/y)"
+        again = raw_input("> ")
+
+main()
